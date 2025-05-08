a46957dae4648e834b1c91457e6ed42522f32856
allanDev9 <allanpenaguerra2003@gmail.com>
2025-05-08 17:11:15 -0600

    diseño de la pagina de inicio

--- src/componets/Layaout/Login.tsx
+++ src/componets/Layaout/Login.tsx
@@ -1,82 +1,114 @@
-import { useNavigate } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import { FormEvent, useState } from 'react';
-import { ProgressSpinner } from 'primereact/progressspinner';
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { FormEvent, useEffect, useState } from "react";
+import { ProgressSpinner } from "primereact/progressspinner";
 
-import { apiTiendita } from '../../services/apiTiendita';
+import { apiTiendita } from "../../services/apiTiendita";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const [username, setUsername] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-    const [message, setMessage] = useState('');
-    const [messageType, setMessageType] = useState<'success' | 'error' | ''>('');
+  const navigate = useNavigate();
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState<"success" | "error" | "">("");
 
-    const handlelogin = async (e: FormEvent) => {
-        e.preventDefault();
+  const handlelogin = async (e: FormEvent) => {
+    e.preventDefault();
 
-
+    useEffect(() => {
+      const fetchUserLogin = async () => {
         try {
-            const response = await apiTiendita.post('user/login', {
-                username,
-                password
-            });
-            setMessage(response.data.message);
-            setMessageType('success')
-            console.log('User', response.data.user);
+          const response = await apiTiendita.post("user/login", {
+            username,
+            password,
+          });
+          setMessage(response.data.message);
+          setMessageType("success");
+          console.log("User", response.data.user);
 
-            localStorage.setItem('username', username)
+          localStorage.setItem("username", username);
 
-            setTimeout(() => {
-                if (username) {
-                    const usuarioLogiado = localStorage.getItem('username') || '';
-                    if (usuarioLogiado === '') {
-                        navigate('/')
-                    } else {
-                        navigate('/home')
-                    }
-                }
-            }, 3000);
-        } catch (error: any) {
-            if (error.response) {
-                setMessage(error.response.data.message);
-            } else {
-                setMessage('Error de conexión al servidor');
+          setTimeout(() => {
+            if (username) {
+              const usuarioLogiado = localStorage.getItem("username") || "";
+              if (usuarioLogiado === "") {
+                navigate("/");
+              } else {
+                navigate("/home");
+              }
             }
-            setMessageType('error')
+          }, 3000);
+        } catch (error: any) {
+          if (error.response) {
+            setMessage(error.response.data.message);
+          } else {
+            setMessage("Error de conexión al servidor");
+          }
+          setMessageType("error");
         }
+      };
 
-    }
+      fetchUserLogin();
+    });
+  };
 
-    return (
-        <div className="flex flex-col justify-center items-center text-center h-screen bg-gray-500">
-            <form onSubmit={handlelogin} className='bg-blue-400 p-25 rounded-xl'>
-                <h1 className="font-bold font-mono text-2xl">Inicia Sesión</h1>
-                <Box
-                    className="flex flex-col text-white"
-                    component="form"
-                    sx={{ m: 5, width: '25ch', gap: 4 }}
-                    noValidate
-                    autoComplete="off"
-                >
-                    <TextField type='text' value={username} onChange={(e) => setUsername(e.target.value)} id="standard-basic" color='success' label="Usuario" variant="standard" />
-                    <TextField className='mt-5' type='password' value={password} onChange={(e) => setPassword(e.target.value)} id="standard-basic" color='success' label="Contraseña" variant="standard" />
-                </Box>
-                <button type='submit' className='bg-black text-white p-3 rounded-xl w-50 font-bold cursor-pointer hover:bg-gray-800'>Iniciar</button>
-                {message && (
-                    <div>
-                        <p className={`font-semibold relative right-0.5 top-5 ${messageType === 'error' ? 'text-red-700' : 'text-green-700'}`}>{message}</p>
-                        {messageType === 'success' && (
-                            <div className='flex justify-center relative top-7'>
-                                <ProgressSpinner style={{ width: '50px', height: '50px' }} />
-                            </div>
-                        )}
-                    </div>
-                )}
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col justify-center items-center text-center h-screen bg-gray-500">
+      <form onSubmit={handlelogin} className="bg-blue-400 p-25 rounded-xl">
+        <h1 className="font-bold font-mono text-2xl">Inicia Sesión</h1>
+        <Box
+          className="flex flex-col text-white"
+          component="form"
+          sx={{ m: 5, width: "25ch", gap: 4 }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            id="standard-basic"
+            color="success"
+            label="Usuario"
+            variant="standard"
+          />
+          <TextField
+            className="mt-5"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="standard-basic"
+            color="success"
+            label="Contraseña"
+            variant="standard"
+          />
+        </Box>
+        <button
+          type="submit"
+          className="bg-black text-white p-3 rounded-xl w-50 font-bold cursor-pointer hover:bg-gray-800"
+        >
+          Iniciar
+        </button>
+        {message && (
+          <div>
+            <p
+              className={`font-semibold relative right-0.5 top-5 ${
+                messageType === "error" ? "text-red-700" : "text-green-700"
+              }`}
+            >
+              {message}
+            </p>
+            {messageType === "success" && (
+              <div className="flex justify-center relative top-7">
+                <ProgressSpinner style={{ width: "50px", height: "50px" }} />
+              </div>
+            )}
+          </div>
+        )}
+      </form>
+    </div>
+  );
+};
 
 export default Login;
--- src/componets/Page/Home.tsx
+++ src/componets/Page/Home.tsx
@@ -1,109 +1,179 @@
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { useNavigate } from 'react-router-dom';
-import LogoutIcon from '@mui/icons-material/Logout';
-import { useState } from 'react';
-import './style.css';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import './colors.css';
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { useNavigate } from "react-router-dom";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { useEffect, useState } from "react";
+import "./style.css";
+import "./colors.css";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+
+import AnuncioImg from "../../assets/Anuncio.png";
+import Prodcutos1Img from "../../assets/ProductosHome1.jpg";
+import Prodcutos2Img from "../../assets/ProductosHome2.webp";
+
+import { apiTiendita } from "../../services/apiTiendita";
 
 const style = {
-    position: 'absolute',
-    textAlign: 'center',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    height: 500,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-    backgroundColor: '#31b3e0'
+  position: "absolute",
+  textAlign: "center",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  height: 500,
+  bgcolor: "rgba(0, 0, 255, 0.5)",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
 };
 
 type ThemeProps = {
-    theme: boolean
-}
+  theme: boolean;
+};
 
 export const Home = ({ theme }: ThemeProps) => {
-    const [open, setOpen] = useState(false);
-    const ThemeMode = localStorage.getItem('theme') === 'dark' || 'light';
-    const navigate = useNavigate();
-    const username = localStorage.getItem('username') || '';
+  const UsersJSON = {
+    name: "",
+    username: "",
+  };
 
-    const handleRedireccionProductos = () => {
-        navigate('/productos')
-    }
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const username = localStorage.getItem("username");
+  const [user, setUser] = useState(UsersJSON);
 
-    const handleLogout = () => {
-        localStorage.removeItem('username')
-        navigate('/login')
-    }
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await apiTiendita.get(`user/${username}`);
+        console.log("Usuarios recibidos:", response.data);
+        setUser(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-    const handleOpenModal = () => {
-        setOpen(true);
+    if (username) {
+      fetchUser();
     }
-    const handleCloseModal = () => {
-        setOpen(false)
-    }
+  }, [username]);
 
-    return (
-        <section className={`${theme ? 'light' : 'dark'}`}>
-            <div className="grid grid-cols-5 grid-rows-5 gap-1 h-[100vh] p-5">
-                <div className="row-span-5 border border-white bg-gray-600 rounded-xl"></div>
-                <div className="col-span-3 border border-white row-span-5 bg-gray-600 overflow-y-scroll diseño-scroll rounded-xl">
-                    <section className='flex flex-col justify-center items-center p-5 font-sans text-white'>
-                        <div className='row-span-5 col-start-5 flex flex-row text-center fixed top-5 border-0 bg-blue-500 w-[57%] py-2 justify-center rounded-xl'>
-                            <h1 className='font-mono text-yellow-300 font-bold relative left-19 top-2 text-2xl'>Mi Tiendita</h1>
-                            <button onClick={handleRedireccionProductos} className='bg-black p-2 relative left-60 rounded-xl px-5'>Ir a la Tiendita<ArrowForwardIcon /></button>
-                        </div>
-                        <div className='self-start mt-20'>
-                            <p>Esta es mi dienta en linea</p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed velit est id blanditiis nulla fugiat delectus facere, dignissimos quae, a nesciunt architecto. Debitis amet perferendis vel nobis alias ea eaque.
-                                Doloribus blanditiis ullam, sapiente deleniti numquam corporis, amet magni atque minima accusamus repudiandae asperiores possimus provident, tempora suscipit nostrum. Earum suscipit in maxime dolor voluptates amet ullam quas! Magni, laudantium!
-                                Officiis veniam dignissimos veritatis odit dolores officia, ducimus perferendis, pariatur adipisci magni rem accusantium explicabo commodi voluptatum, amet maxime doloribus maiores nostrum ratione voluptatem blanditiis tempora delectus nihil! Suscipit, reiciendis?
-                                Modi, ex eum! Laudantium voluptatibus voluptates maiores, assumenda dolor atque veniam esse alias incidunt fugiat iste vero veritatis enim. Rerum ratione numquam vitae assumenda at possimus molestiae quis quam ipsum!
-                                Doloribus blanditiis ullam, sapiente deleniti numquam corporis, amet magni atque minima accusamus repudiandae asperiores possimus provident, tempora suscipit nostrum. Earum suscipit in maxime dolor voluptates amet ullam quas! Magni, laudantium!
-                                Officiis veniam dignissimos veritatis odit dolores officia, ducimus perferendis, pariatur adipisci magni rem accusantium explicabo commodi voluptatum, amet maxime doloribus maiores nostrum ratione voluptatem blanditiis tempora delectus nihil! Suscipit, reiciendis?
-                                Modi, ex eum! Laudantium voluptatibus voluptates maiores, assumenda dolor atque veniam esse alias incidunt fugiat iste vero veritatis enim. Rerum ratione numquam vitae assumenda at possimus molestiae quis quam ipsum!
-                                Doloribus blanditiis ullam, sapiente deleniti numquam corporis, amet magni atque minima accusamus repudiandae asperiores possimus provident, tempora suscipit nostrum. Earum suscipit in maxime dolor voluptates amet ullam quas! Magni, laudantium!
-                                Officiis veniam dignissimos veritatis odit dolores officia, ducimus perferendis, pariatur adipisci magni rem accusantium explicabo commodi voluptatum, amet maxime doloribus maiores nostrum ratione voluptatem blanditiis tempora delectus nihil! Suscipit, reiciendis?
-                                Modi, ex eum! Laudantium voluptatibus voluptates maiores, assumenda dolor atque veniam esse alias incidunt fugiat iste vero veritatis enim. Rerum ratione numquam vitae assumenda at possimus molestiae quis quam ipsum!
-                            </p>
-                        </div>
-                    </section>
-                </div>
-                <div className="col-span-1 border border-white row-span-5 bg-gray-600 rounded-xl">
-                    <div className='flex flex-row font-extrabold justify-around mt-5 bg-blue-700 w-65 h-10 relative left-1.5 items-center'>
-                        <div onClick={handleOpenModal}>
-                            <span className='text-yellow-300 cursor-pointer'>Usuario: <span className='text-white'>{username}</span></span>
-                        </div>
-                        <div onClick={handleLogout} className='cursor cursor-pointer text-yellow-300 hover:text-white transition duration-300 ease-in-out'>
-                            <LogoutIcon />
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <Modal
-                        open={open}
-                        onClose={handleCloseModal}
-                        aria-labelledby="modal-modal-title"
-                        aria-describedby="modal-modal-description"
-                    >
-                        <Box sx={style}>
-                            <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ color: 'white' }}>
-                                <span className='text-black'>Perfil:</span> {username}
-                            </Typography>
-                            <div className='border-1 border-b-black mt-5'></div>
-                            <Typography id="modal-modal-description" sx={{ mt: 2, textAlign: 'start' }}>
-                                Datos del usuario...
-                            </Typography>
-                        </Box>
-                    </Modal>
-                </div>
+  const handleRedireccionProductos = () => {
+    navigate("/productos");
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("username");
+    navigate("/login");
+  };
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
+
+  return (
+    <section className="bg-black">
+      <div className="grid grid-cols-5 grid-rows-5 gap-1 h-[100vh] p-5">
+        <div className="row-span-5 border border-white bg-gray-600 rounded-xl">
+          <img
+            className="w-[100%] h-[94vh]"
+            src={AnuncioImg}
+            alt="Imagen del Anuncio"
+          />
+        </div>
+        <div className="col-span-3 border border-white row-span-5 bg-gray-600 overflow-y-scroll diseño-scroll rounded-xl">
+          <section className="flex flex-col justify-center items-center p-5 font-sans text-white">
+            <div className="row-span-5 col-start-5 flex flex-row text-center fixed top-5 border-0 bg-blue-500 w-[57%] py-2 justify-center rounded-xl">
+              <h1 className="font-mono text-yellow-300 font-bold relative left-19 top-2 text-2xl">
+                Mi Tiendita
+              </h1>
+              <button
+                onClick={handleRedireccionProductos}
+                className="bg-black p-2 relative left-60 rounded-xl px-5"
+              >
+                Ir a la Tiendita
+                <ArrowForwardIcon />
+              </button>
             </div>
-        </section>
-    )
-}
+            <div className="self-start mt-20">
+              <p className="text-blue-300 font-bold text-xl">
+                ¡Bienvenidos a Tu Tiendita de Confianza!
+              </p>
+              <p>
+                En nuestra tiendita encontrarás todo lo que necesitas para el
+                hogar y el día a día: productos frescos, artículos de limpieza,
+                snacks, bebidas, y mucho más. Nos enorgullece ofrecer atención
+                cercana, buenos precios y un servicio rápido. Tu satisfacción es
+                nuestra prioridad. ¡Gracias por apoyarnos y ser parte de nuestra
+                comunidad!
+              </p>
+              <div className="grid grid-cols-2 gap-x-5 mt-10">
+                <p>
+                  <img
+                    className="w-[100%] h-[100%]"
+                    src={Prodcutos1Img}
+                    alt="Imagen de producto 1"
+                  />
+                </p>
+                <p>
+                  <img src={Prodcutos2Img} alt="Imagen de producto 2" />
+                </p>
+              </div>
+            </div>
+          </section>
+        </div>
+        <div className="col-span-1 border border-white row-span-5 bg-gray-600 rounded-xl">
+          <div className="flex flex-row font-extrabold justify-around mt-5 bg-blue-700 w-65 h-10 relative left-1.5 items-center">
+            <div onClick={handleOpenModal}>
+              <span className="text-yellow-300 cursor-pointer">
+                Usuario: <span className="text-white">{username}</span>
+              </span>
+            </div>
+            <div
+              onClick={handleLogout}
+              className="cursor cursor-pointer text-yellow-300 hover:text-white transition duration-300 ease-in-out"
+            >
+              <LogoutIcon />
+            </div>
+          </div>
+        </div>
+        <div>
+          <Modal
+            open={open}
+            onClose={handleCloseModal}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <section id="modal-modal-title" style={{ color: "white" }}>
+                <h1 className="text-yellow-300 font-bold">Datos del Usuario</h1>
+              </section>
+              <div className="border-1 border-b-white mt-5"></div>
+              <section
+                id="modal-modal-description"
+                style={{
+                  marginTop: 20,
+                  textAlign: "start",
+                  fontWeight: "bold",
+                  color: "yellow",
+                }}
+              >
+                <li className="flex flex-col">
+                  <span>
+                    Nombre: <span className="text-white">{user.name}</span>
+                  </span>
+                  <span>
+                    Usuario: <span className="text-white">{user.username}</span>
+                  </span>
+                </li>
+              </section>
+            </Box>
+          </Modal>
+        </div>
+      </div>
+    </section>
+  );
+};
