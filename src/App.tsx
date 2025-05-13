4616ff16c3beedfe258ab828a396122ebbbbcd83
allanDev9 <allanpenaguerra2003@gmail.com>
2025-05-13 17:43:47 -0600

    change page Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './componets/Layaout/Login';
-import ProductosPage from './componets/Page/ProductosPage';
-import { Home } from './componets/Page/Home';
-import PrivateRoute from './componets/PrivateRoute';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./componets/Layaout/Login";
+import ProductosPage from "./componets/Page/ProductosPage";
+import { Home } from "./componets/Page/Home";
+import PrivateRoute from "./componets/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path="/" element={<Login />} />
 
         {/* Protección de rutas*/}
         <Route element={<PrivateRoute />}>
-          <Route path='/login' element={<Login />} />
-          <Route path='/home' element={<Home theme={true} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/productos" element={<ProductosPage />} />
         </Route>
       </Routes>
-
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
